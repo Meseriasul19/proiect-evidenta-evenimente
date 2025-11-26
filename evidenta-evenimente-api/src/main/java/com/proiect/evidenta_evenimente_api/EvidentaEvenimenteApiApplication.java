6da5ee0bf9c48f2c7133c2c46ac140879c303815
Calimoceanu Razvan <calimoceanurazvan11@gmail.com>
2025-11-26 19:18:00 +0200

    first commit

---
+++ evidenta-evenimente-api/src/main/java/com/proiect/evidenta_evenimente_api/EvidentaEvenimenteApiApplication.java
@@ -0,0 +1,13 @@
+package com.proiect.evidenta_evenimente_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvidentaEvenimenteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvidentaEvenimenteApiApplication.class, args);
+	}
+
+}
---
+++ evidenta-evenimente-api/src/test/java/com/proiect/evidenta_evenimente_api/EvidentaEvenimenteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiect.evidenta_evenimente_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvidentaEvenimenteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
